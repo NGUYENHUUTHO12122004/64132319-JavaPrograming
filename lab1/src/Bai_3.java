91027a7458db9a83b438ef9e07b865f21c89654d
NGUYENHUUTHO12122004 <tho.nh.64cntt@ntu.edu.vn>
2025-02-17 10:40:32 +0700

    Lap 1

--- lab1/src/Bai_3.java
+++ lab1/src/Bai_3.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class Bai_3 {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập cạnh của khối lập phương: ");
+        double canh = scanner.nextDouble();
+        
+        double theTich = Math.pow(canh, 3);
+        
+        System.out.println("\nThông tin khối lập phương:");
+        System.out.println("Thể tích: " + theTich);
+        
+        scanner.close();
+    }
 }
